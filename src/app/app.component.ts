a8ede7a96793c689846c7ad8f0a2838eb3b89e36
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-09-28 19:21:39 -0300

    feat: first component and function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'novo';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log("Hello World")
+  }
+  title='novo';
+
+  function(): void{
+
+  }
 }
