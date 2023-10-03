cfd3e295a787f320b7ac8846bf33d20e2028a6b1
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-10-02 21:07:21 -0300

    feat: productForm implementation

--- src/app/components/register-product/register-product.component.ts
+++ src/app/components/register-product/register-product.component.ts
@@ -1,35 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductType } from 'src/app/enums/product-type.enum';
 
 @Component({
   selector: 'app-register-product',
   templateUrl: './register-product.component.html',
   styleUrls: ['./register-product.component.scss']
 })
-export class RegisterProductComponent implements OnInit{
-  // Declarar o formulário
+export class RegisterProductComponent implements OnInit {
   public productForm: FormGroup;
+  productTypes = Object.values(ProductType); // Assuming ProductType is correctly imported
 
-  constructor(private formBuilder: FormBuilder) {};
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    // Chamar a função para executar
     this.createForm();
   }
 
-  // Começa o formulário dentro do TypeScript
-  // Criar uma função que cria o formulário
   private createForm() {
-    // 1 - Chamar o atributo do formulário
-    // 2 - Construir os atributos do formulário
-    this.productForm = this.formBuilder.group({
-      // inputs(atributos) do formulário
-      title: '',
-      author: '',
-      year: '',
-      price: 0
-    })
+    this.productForm = this.fb.group({
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      code: ['', [Validators.required]],
+      costPrice: ['', [Validators.required, Validators.pattern('^[0-9]+(\.[0-9]{1,2})?$')]],
+      salePrice: ['', [Validators.required, Validators.pattern('^[0-9]+(\.[0-9]{1,2})?$')]],
+      preparationTime: ['', [Validators.required]],
+      productType: ['', []],
+      comments: ['', []]
+    });
+  }
 
-    // 3 - Criar no HTML o formulário
+  sendForm() {
+    if (this.productForm.valid) {
+      console.log(this.productForm.value);
+      // Implement your form submission logic
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  formatMoney(event: any) {
+    const value = event.target.value;
+    event.target.value = parseFloat(value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); // Apply formatting
+  }
+  
+  unformatMoney(event: any) {
+    event.target.value = event.target.value.replace(/,/g, ''); // Remove formatting
   }
 }
---
+++ src/app/enums/product-type.enum.ts
@@ -0,0 +1,5 @@
+export enum ProductType {
+  SNACK = "SNACK",
+  DRINK = "DRINK",
+  DESSERT = "DESSERT"
+}
